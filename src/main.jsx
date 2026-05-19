e1b15c00e58f5a5352ab6894b6a08097f50baff2
maryam <marlyedu001@gmail.com>
2026-05-19 12:01:17 -0400

    delete content of template files and import bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '.styles/main.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM(document.getElementById('root')).render(
+  <React>
     <App />
-  </StrictMode>,
+  </React>,
 )
