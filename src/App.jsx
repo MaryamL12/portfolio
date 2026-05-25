493354407910fcd67de16a5191fa0b7aec705264
maryam <marlyedu001@gmail.com>
2026-05-24 22:41:09 -0400

    experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Profile from './components/Profile'
 import About from './components/About'
+import Experience from './components/Experience'
 import Projects from './components/Projects'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Profile />
       <About />
+      <Experience />
       <Projects />
     </>
   )
